01b3af37221abdbba582dcbfebcfa549eac556f1
Carlos <charliedn@Gmail.com>
2018-09-15 11:40:45 +0100

    Add Controller Concert to create AuthToken

---
+++ app/controllers/concerns/generate_token.rb
@@ -0,0 +1,39 @@
+require 'twilio-ruby'
+
+module GenerateToken
+  extend ActiveSupport::Concern
+
+
+  def generate_token
+
+    identity = "User"
+
+    video_grant = Twilio::JWT::AccessToken::VideoGrant.new
+    video_grant.room = "Cool Room"
+
+
+    token = Twilio::JWT::AccessToken.new(
+      Rails.application.secrets.twilio_account_sid,
+      Rails.application.secrets.twilio_api_key,
+      Rails.application.secrets.twilio_api_secret,
+      [video_grant],
+      identity: identity
+      )
+
+    puts token.to_jwt
+
+
+  end
+
+
+
+
+# twilio_account_sid = "AC17f633924f819879c5f1f83877f3ef85"
+# twilio_auth_token  = "2934e990f32c5271363bf8fc90bad52d"
+# #video
+# twilio_api_key     = "SK599de60338e1a46dee5a20323878a68e"
+# twilio_api_secret  = "SJIpoit5p3uWRbBvwIwKDWSj9Y8IyCX7"
+
+
+
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  include GenerateToken
+  before_action :create_token
+
   def index
+  end
+
+  private
+
+  def create_token
+   @token = generate_token
+   puts @token
   end
 end
