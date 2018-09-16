0d66a8d0a2ef1c2af4f2e849dd7558ad8234bea6
Carlos <charliedn@Gmail.com>
2018-09-16 20:55:00 +0100

    save

---
+++ app/services/twilio_sms.rb
@@ -0,0 +1,16 @@
+class TwilioSms
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+
+  def call
+    client = Twilio::REST::Client.new
+    client.messages.create({
+      from: Rails.application.secrets.twilio_number,
+      to: "+447549273987",
+      body: message
+    })
+  end
+end
