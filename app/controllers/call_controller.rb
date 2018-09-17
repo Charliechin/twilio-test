f1231b99627d2b6845e8b16eaa3701f686fbcdce
Carlos <charliedn@Gmail.com>
2018-09-17 16:04:02 +0100

    Calls student

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -1,5 +1,26 @@
 class CallController < ApplicationController
+ skip_before_action :verify_authenticity_token
+# before_action :authenticate_user!
+
   def connect
-    binding.pry
+    render xml: twilio_reponse
   end
+
+  private
+
+  def twilio_reponse
+    twilio_number = Rails.application.secrets.twilio_number
+    res = Twilio::TwiML::VoiceResponse.new do |response|
+    dial = Twilio::TwiML::Dial.new caller_id: twilio_number
+      if params.include?(:phone_number)
+        dial.number params[:phone_number]
+      else
+        # call some other number
+        dial.number("447549273987")
+      end
+      response.append(dial)
+    end
+    return res.to_s
+  end
+
 end
--- app/services/twilio_capability_token_service.rb
+++ app/services/twilio_capability_token_service.rb
@@ -1,17 +1,18 @@
 class TwilioCapabilityTokenService
-#  TwilioSmsService.new("your message here").call
+  #  TwilioSmsService.new("your message here").call
+  attr_reader :role
 
-  def initialize
+  def initialize(role)
+    @role = role
   end
 
   def call
     capability = Twilio::JWT::ClientCapability.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
-    outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(Rails.application.secrets.twilio_application_sid, role)
+    outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(Rails.application.secrets.twilio_api_sid, @role)
     capability.add_scope outgoing_scope
 
     incoming_scope = Twilio::JWT::ClientCapability::IncomingClientScope.new(role)
     capability.add_scope incoming_scope
-    binding.pry
-    capability
+    capability.to_s
   end
 end
